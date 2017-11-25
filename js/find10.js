3b7ed6f1c15d13e7f0c82accaaaf7a2aa8931d49
samuelchiu20050427 <samuelchiu6@gmail.com>
2017-11-25 11:55:51 +0800

    Add files via upload

---
+++ js/find10.js
@@ -0,0 +1,32 @@
+
+	
+function show(){
+	var data = Collection.find({},{
+		$orderBy:{date:-1},
+		$limit:10
+	});
+	console.log(data);
+	for(var i=0;i<data.length;i++)
+	{
+		$("#myTbody").append(
+			"<tr>"+
+				"<td>"+(i+1) +"</td>"+
+				"<td>"+data[i].date+"</td>"+
+				"<td>"+data[i].kind+"</td>"+
+				"<td>"+data[i].money+"</td>"+
+				"<td>"+data[i].words+"</td>"+
+			"</tr>"
+		)
+	}
+	
+
+
+
+	
+}
+
+
+var fdb = new ForerunnerDB();
+var myDb = fdb.db("myDb");
+var Collection = myDb.collection('Collection');
+Collection.load(show);
--- js/main.js
+++ js/main.js
@@ -1,7 +1,31 @@
-function myfunction(){
-	var text = $("#my-select").val();
-	alert(text);
+
+
+function show(){
+	var data = Collection.find();
+	console.log(data);
 }
 
 
+var fdb = new ForerunnerDB();
+var myDb = fdb.db("myDb");
+var Collection = myDb.collection('Collection');
+Collection.load(show);
+
+function myfunction(){
+
+	Collection.insert({
+		text:$("#my-select").val(),
+		date:$(".date").val(),
+		kind: $(".kind").val(),
+		money: $(".money").val(),
+		words:$(".words").val()
+
+	})
+	Collection.save();
+	$("#my-select").val(""),
+	$(".date").val(""),
+	$(".kind").val(""),
+	$(".money").val(""),
+	$(".words").val("")
+}
 $("#submit").on("click",myfunction	);
